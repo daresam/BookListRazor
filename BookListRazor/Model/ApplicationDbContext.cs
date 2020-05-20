bc2c6e3cbd63ce5535767895495f937bc0da8a8c
Dare Sam <dawakesam@gmail.com>
2020-05-20 22:07:06 +0100

    Create Book List

--- BookListRazor/Model/ApplicationDbContext.cs
+++ BookListRazor/Model/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace BookListRazor.wwwroot
+namespace BookListRazor.Model
 {
     public class ApplicationDbContext: DbContext
     {
